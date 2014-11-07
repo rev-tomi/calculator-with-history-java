c5aca353cc0f37ccac0019aabbf27f9e0de15480
Your Name <you@example.com>
2014-11-07 23:52:28 +0100

    refactoring test

--- src/test/java/com/example/calculator/CalculatorOperatorTest.java
+++ src/test/java/com/example/calculator/CalculatorOperatorTest.java
@@ -6,24 +6,29 @@ import org.junit.Test;
 
 public class CalculatorOperatorTest extends Calculator {
 	
-	private boolean getAddOperationCalled;
-
 	@Test
 	public void testGettingTheRightOperation() {
 		// GIVEN
-		Calculator calculator = new Calculator();
+		AddCalledCalculator calculator = new AddCalledCalculator();
 		
 		// WHEN
 		calculator.add(2, 3);
 		
 		// THEN
-		assertTrue(getAddOperationCalled);
+		assertTrue(calculator.getAddOperationCalled);
 	}
 	
-	@Override
-	protected Operation getAddOperation(int a, int b) {
-		getAddOperationCalled = true;
-		return null;
+	
+	private static class AddCalledCalculator extends Calculator {
+		
+		private boolean getAddOperationCalled;
+		
+		@Override
+		protected Operation getAddOperation(int a, int b) {
+			getAddOperationCalled = true;
+			return null;
+		}
+		
 	}
 	
 }
